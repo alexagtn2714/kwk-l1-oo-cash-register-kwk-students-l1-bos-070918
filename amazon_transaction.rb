86bb9496269e0563bbf647db3f3fcfdd41d76529
alexagtn2714 <alexagaton@hotmail.com>
2018-07-16 13:38:13 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -17,7 +17,7 @@ class AmazonTransaction
   
   def apply_discount
     if discount == 20 
-    @total = total - @total * @discount/100.to_f 
+    @total = total - @total * @discount/100
     "After the discount, the total comes to $800."
   end 
   end 
